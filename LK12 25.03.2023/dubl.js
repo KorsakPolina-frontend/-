a02b6f1162ba5daed8a09cc610f9a08b41d6a38a
Полина <121341871+KorsakPolina-frontend@users.noreply.github.com>
2023-04-05 00:25:21 +0300

    Исправила ошибки, добавила модальное окно.

--- LK12 25.03.2023/dubl.js
+++ LK12 25.03.2023/dubl.js
@@ -1,9 +1,21 @@
+let myCat = {
+    name: "Арбуз",
+    age: 3,
+    image: "https://proprikol.ru/wp-content/uploads/2019/08/kartinki-nyashnye-kotiki-27.jpg",
+    favorite: false
+}
 
+let myCat2 = {
+    name: "Персик",
+    favorite: true
+}
+
+const box = document.querySelector(".container");
 function createCard(cat, el = box) {
     const card = document.createElement("div");
     card.className = "card";
     if (!cat.image) {
-        card.classList.add("default");
+    card.classList.add("default");
     } else {
         card.style.backgroundImage = `url(${cat.image})`;
     }
@@ -16,13 +28,13 @@ function createCard(cat, el = box) {
         e.stopPropagation();
         if (cat.id) {
             fetch(`${path}/update/${cat.id}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({favorite: !cat.favorite})
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({favorite: !cat.favorite})
             })
-            .then(res => {
+            .then (res => {
                 if (res.status === 200) {
                     like.classList.toggle("fa-solid");
                     like.classList.toggle("fa-regular");
@@ -37,49 +49,52 @@ function createCard(cat, el = box) {
         card.append(age);
     }
     //card.addEventListener("click", (e) => {
-    //    deleteCard(cat.id, card);
-    //});
+      //  deleteCard(cat.id, card);
+   // });
     el.append(card);
 }
+// Удаление кота при нажатии на карточку
 
 function deleteCard(id, el) {
     if (id) {
         fetch(`${path}/delete/${id}`, {
             method: "delete"
         })
-            .then(res => {
-                // console.log(res);
-                // console.log(res.status);
-                if (res.status === 200) {
-                    el.remove();
-                }
-            })
+        .then(res => {
+           // console.log(res);
+           // console.log(res.status);
+           if (res.status === 200) {
+            el.remove();
+           }
+        })
     }
 }
-
-
+/*
+createCard(myCat);
+createCard(myCat2);
+*/
+const user = "polinakorsak"
+const path = `https://cats.petiteweb.dev/api/single/${user}`;
 
 fetch(path + "/show")
     .then(function(res) {
         console.log(res);
         if (res.statusText === "OK") {
-            /*
-                Все методы res возвращают Promise
-                res.text() => возвращает текстовое содержимое (HTML-файл)
-                res.blob() => возвращает двоичный код (бинарный формат данных) 10 => 00001010 => 0a => файлы (картинки / файл)
-                res.json() => отображает данные в ввиде объекта
-            */
             return res.json();
         }
     })
     .then(function(data) {
         // data - отввет от сервера
         // console.log(data);
-        for (let c of data) {
-            createCard(c, box);
+        if (!data.length) {
+            box.innerHTML = "<div class=\"empty\">У вас пока еще нет питомцев</div>"
+        } else {
+            for (let c of data) {
+                createCard(c, box);
+            }
         }
     })
-
+/*
 let ids = [];
 fetch(path + "/ids")
     .then(res => res.json())
@@ -89,10 +104,10 @@ fetch(path + "/ids")
         myCat.id = ids.length ? ids[ids.length - 1] + 1 : 1;
         addCat(myCat);
     })
-    
+    */
     
    //Добавление кота через Js
-/*
+
       function addCat(cat) {
         fetch(path + "/add", {
             method: "POST",
@@ -106,5 +121,5 @@ fetch(path + "/ids")
             console.log(data);
         }) 
     }
-    */
+    
     addCat(myCat);    
